b3436a02d18a92565745a4d66c363b1472d9fcc3
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-03-18 20:46:23 -0400

    implement navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from "../styles/NavBar.module.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>
-      <p>NavBar</p>
-    </div>
+    <nav>
+      <h1>Puppy Bowl</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/new-player">New Player</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
